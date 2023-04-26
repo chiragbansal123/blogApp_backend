37aaaa3028478ad23c9ed1e4e50807cfcdc26cda
chiragbansal123 <47918090+chiragbansal123@users.noreply.github.com>
2023-04-26 15:14:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://chiragbansal123:O3b4811jzoCDcWyW@cluster0.lne9m.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://chiragbansal123:OPhfrCvcYpV5F9pM@cluster0.lne9m.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
